52e49d6f90f0b814cb65ef8738f19a1239debde3
will-beta <i@will-beta.com>
2022-06-17 23:37:31 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+
 if [[ -v DEV_CODESPACE_TOKEN ]]; then
   git config --global url."https://$DEV_CODESPACE_TOKEN@github.com/".insteadOf "https://github.com/"
 fi
+
+
+cd /
+curl https://raw.githubusercontent.com/nektos/act/master/install.sh | sudo bash
