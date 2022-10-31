0aae49f81409dc5d5d6f0b18256a0dc77368b3de
will-beta <i@will-beta.com>
2022-10-31 10:46:38 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,10 @@ if [[ -v DEV_CODESPACE_TOKEN ]]; then
   git config --global url."https://$DEV_CODESPACE_TOKEN@github.com/".insteadOf "https://github.com/"
 fi
 
+if [[ -v GITLAB_FOSUN_COM_TOKEN ]]; then
+  git config --global url."https://$GITLAB_FOSUN_COM_TOKEN@gitlab.fosun.com/".insteadOf "https://gitlab.fosun.com/"
+fi
+
 
 cd /
 curl https://raw.githubusercontent.com/nektos/act/master/install.sh | sudo bash
